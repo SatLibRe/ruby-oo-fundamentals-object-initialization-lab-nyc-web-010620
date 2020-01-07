dad087ee7262923e21a6274b3f03e3ff09e2f0b5
Miles Higbie <miles.higbie@gmail.com>
2020-01-07 14:44:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,21 @@
 class Person 
   def initialize(person_name)
-    @name 
+    @name  = name 
   end 
 end 
+
+
+
+# class Dog
+#   def initialize(breed)
+#     @breed = breed
+#   end
+ 
+#   def breed=(breed)
+#     @breed = breed
+#   end
+ 
+#   def breed
+#     @breed
+#   end
+# end
